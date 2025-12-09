0106aeca16e2d499e6f22d00cc789ac6f0b96051
changwoo-bear <139298099+changwoo-bear@users.noreply.github.com>
2025-12-09 15:26:59 +0900

    Fix motor_report: firmware sends arg1=Right, arg2=Left per motor-packet.md (#10)

--- projects/FW-BaseWrapper/src/motor.cpp
+++ projects/FW-BaseWrapper/src/motor.cpp
@@ -98,16 +98,17 @@ void Motor::messageReceivedCallback(std::shared_ptr<void> packet, int size) {
 
     ::example_node::MotorReport report;
 
-    report.RPM_L = ntohf(motor_packet->arg1.f32);
-    if (std::abs(report.RPM_L) < RPM_THRESHOLD) {
-      report.RPM_L = 0.0f;
-    }
-    report.RPM_R = ntohf(motor_packet->arg2.f32);
+    // Note: Firmware sends arg1=Right, arg2=Left (per motor-packet.md documentation)
+    report.RPM_R = ntohf(motor_packet->arg1.f32);
     if (std::abs(report.RPM_R) < RPM_THRESHOLD) {
       report.RPM_R = 0.0f;
     }
-    report.status_L = ntohl(motor_packet->arg3.u32);
-    report.status_R = ntohl(motor_packet->arg4.u32);
+    report.RPM_L = ntohf(motor_packet->arg2.f32);
+    if (std::abs(report.RPM_L) < RPM_THRESHOLD) {
+      report.RPM_L = 0.0f;
+    }
+    report.status_R = ntohl(motor_packet->arg3.u32);
+    report.status_L = ntohl(motor_packet->arg4.u32);
     motor_report_pub_.publish(report);
     break;
   }
